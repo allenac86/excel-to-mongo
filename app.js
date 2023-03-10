498952799b70be672d7b039d4ed4b61c14833213
Adam Allen <allenac86@gmail.com>
2023-03-09 20:49:49 -0500

    refactor with more meaningful variable names

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 require('dotenv').config();
 const connect = require('./db/db');
 const excelFile = require('read-excel-file/node');
-const lodash = require('lodash');
+const _ = require('lodash');
 const DataModel = require('./model/DataModel');
-const { Mongoose, connection } = require('mongoose');
+const { connection } = require('mongoose');
 
-let recordsArray = [];
-let propsArray = [];
-let collection = [];
+let columnHeadingList = [];
+let rowDataList = [];
 
-const massageData = (excelData) => {
-	const excelDataCopy = [...excelData];
+const separateColumnHeadingsFromRowData = (excelData) => {
+	const excelDataArray = [...excelData];
 
-	propsArray = excelDataCopy.shift();
-	recordsArray = excelDataCopy;
-	return recordsArray;
+	columnHeadingList = excelDataArray.shift();
+
+	return excelDataArray;
 };
 
-const createCollection = (rowDataArray) => {
+const createRowDataList = (rowDataArray) => {
 	rowDataArray.forEach((record) => {
-		const document = lodash.zipObject(propsArray, record);
+		const document = _.zipObject(columnHeadingList, record);
 
-		collection.push(document);
+		rowDataList.push(document);
 	});
 };
 
@@ -31,38 +30,36 @@ const convertExcelToMongoCollection = (path) => {
 	excelFile(path)
 		.then((rows) => {
 			originalExcelData = rows;
-			createCollection(massageData(originalExcelData));
+			createRowDataList(separateColumnHeadingsFromRowData(originalExcelData));
 		})
 		.catch((error) => console.log(error));
 
-	console.log("File converted ...");
+	console.log('File converted ...');
 };
 
 const start = async () => {
-
 	try {
 		await connect(process.env.MONGO_URI)
 			.then(convertExcelToMongoCollection(process.env.FILE_PATH))
 			.catch((error) => console.log(error.message));
 
-		DataModel.insertMany(collection)
+		DataModel.insertMany(rowDataList)
 			.then(() => {
 				console.log('Collections inserted ...');
 			})
 			.then(() => {
-				console.log("Operation complete ...");
+				console.log('Operation complete ...');
 			})
 			.catch((error) => console.log(error.message))
 			.finally(() => {
 				connection.close(false, () => {
-					console.log("Database connection closed");
+					console.log('Database connection closed');
 					process.exit(0);
 				});
 			});
 	} catch (err) {
 		console.error(err);
 	}
-
 };
 
 start();
